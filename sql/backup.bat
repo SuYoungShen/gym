13db777a22b464e7db0464b5c38b741ccee1c833
unknown <k90218104@gcloud.csu.edu.tw>
2018-06-11 00:05:34 +0800

    備份用檔案 in 20180611

---
+++ sql/backup.bat
@@ -0,0 +1,19 @@
+@echo off
+set nowYear=%date:~0,4%
+set nowMonth=%date:~5,2%
+set nowDay=%date:~8,2%
+set nowHr=%time:~0,2%
+
+if %nowHr% LSS 10 set nowHr=0%nowHr%
+set nowMin=%time:~3,2%
+
+set NewFileName=Log_File_%nowYear%%nowMonth%%nowDay%%nowHr%%nowMin%.sql
+
+set hour=%time: =0%
+set LogFile=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%%hour:~0,2%%TIME:~3,2%%TIME:~6,2%.sql
+
+cd D:\file\xampp\mysql\bin
+
+mysqldump -h localhost -u root -p --all-databases > D:\%LogFile%
+
+pause
