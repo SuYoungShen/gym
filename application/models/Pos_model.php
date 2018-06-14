878e6c07a0f28b344a2f436f639ff33073aa8efe
unknown <k90218104@gcloud.csu.edu.tw>
2018-06-15 01:03:12 +0800

    新增資料功能 in 20180615

--- application/models/Pos_model.php
+++ application/models/Pos_model.php
@@ -8,5 +8,27 @@ class Pos_model extends CI_Model {
   public function get_once($table, $where){
     return $this->db->get_where($table, $where)->row();//以class方式呈現
   }
+
+  //新增資料，$table=資料表；$data=資料；$date=日期欄位
+  public function insert($table, $data, $date){
+    $date = $this->date($date);
+    $data = array_merge($data, $date);//array_merge=類似array_push，差別於merge可加key
+    return $this->db->insert($table, $data);
+  }
+
+  
+  public function date($date){
+    date_default_timezone_set("Asia/Taipei");
+    $res = array();
+
+    foreach ($date as $key => $value) {
+      if ($key == 0) {
+        $res[$value] = date('Y-m-d');
+      }else if($key == 1){
+        $res[$value] = date('H:i:s');
+      }
+    }
+    return $res;
+  }
 }
 ?>
