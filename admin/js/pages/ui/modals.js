62b5f46950814829195b92d4ff8fd46662add7bf
unknown <k90218104@gcloud.csu.edu.tw>
2018-06-17 17:26:16 +0800

    新增會員、員工彈跳視窗；上傳圖片並顯示功能 in20180617

--- admin/js/pages/ui/modals.js
+++ admin/js/pages/ui/modals.js
@@ -13,3 +13,36 @@ $(function () {
     $('#mdModal').modal('show');
   });
 });
+
+$(function () {
+  $('.member').on('click', function () {
+    var color = $(this).data('color');
+    $('#largeModal .modal-content').removeAttr('class').addClass('modal-content modal-col-' + color);
+    $('#largeModal').modal('show');
+  });
+});
+
+$(function () {
+  $('.staff').on('click', function () {
+    var color = $(this).data('color');
+    $('#largeModal .modal-content').removeAttr('class').addClass('modal-content modal-col-' + color);
+    $('#largeModal').modal('show');
+  });
+});
+// 上傳圖片並顯示 in 20180617
+function m_readURL(input) {
+
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#m_blah').attr('src', e.target.result).addClass('img-responsive').css('width', '50%');
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#m_imgInp").change(function() {
+  m_readURL(this);
+});
