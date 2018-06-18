bf7d5026236aba403be35a6cb781e75fe20b99cd
unknown <k90218104@gcloud.csu.edu.tw>
2018-06-18 23:52:03 +0800

    新增員工查詢功能 in 20180618

--- application/controllers/Api_console.php
+++ application/controllers/Api_console.php
@@ -41,5 +41,31 @@ class Api_console extends CI_Controller {
 
     }
   }
+
+  // 員工專區
+  public function staff(){
+
+    if($this->input->post('rule') == "select"){
+      $id = $this->input->post('id');
+      $where = "id =".'"'.$id.'"';
+      $data = $this->console_model->get_once('staff', $where);
+      $this->output->set_content_type('application/json')->set_output(json_encode($data));
+    }
+
+    if ($this->input->post('rule') == "delete") {
+      $id = $this->input->post('id');
+      $where = "id=".'"'.$id.'"';
+      if($this->console_model->delete_once('discount_program', $where)){
+        $view_data['code'] = 200;
+        $view_data['msg'] = "刪除成功";
+      }else {
+        $view_data['code'] = 404;
+        $view_data['msg'] = "刪除失敗";
+      }
+      $this->output->set_content_type('application/json')->set_output(json_encode($view_data));
+
+    }
+  }
+
 }
 ?>
