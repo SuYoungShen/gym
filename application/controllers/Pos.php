13b46d1bacfd1a35128e36d8f53f043a74113858
unknown <k90218104@gcloud.csu.edu.tw>
2018-07-08 21:25:07 +0800

    修正提示下次付款與剩餘幾天的判斷 in 20180708

--- application/controllers/Pos.php
+++ application/controllers/Pos.php
@@ -36,7 +36,7 @@ class Pos extends CI_Controller {
           //查詢member與進出場時間資料，取出最後一筆 in 20180615
           $where = "m.card_id ="."'".$card_id."' AND io.who="."'".$card_id."' Order By io.in_date DESC, io.in_time DESC limit 1";
           $data = $this->pos_model->get_once('member as m, in_and_out as io', $where);
-          $remain_day = (strtotime($data->end_contract)-strtotime($data->start_contract))/3600/24;// 剩餘天數
+          $remain_day = (strtotime($data->end_contract)-strtotime($data->next_pay))/3600/24;// 剩餘天數
           // 3600 = 小時;24 = 天
           if ($remain_day <= 31) {
             $view_data['code'] = 500;
@@ -83,11 +83,12 @@ class Pos extends CI_Controller {
           $where = "m.card_id ="."'".$card_id."' AND io.who="."'".$card_id."' Order By io.in_date DESC , io.in_time DESC limit 1";
           $data = $this->pos_model->get_once('member as m, in_and_out as io', $where);
 
-          $remain_day = (strtotime($data->end_contract)-strtotime($data->start_contract))/3600/24;// 剩餘天數
+          $remain_day = (strtotime($data->end_contract)-strtotime($data->next_pay))/3600/24;// 剩餘天數
           // 3600 = 小時;24 = 天
           if ($remain_day <= 31) {
             $view_data['code'] = 500;
-            $view_data['msg'] = "會籍時間快過期囉!剩餘".$remain_day."天";
+            $view_data['msg'] = "會籍時間快過期囉!剩餘".$remain_day."天，下次繳款日：".$data->next_pay;
+
           }
           $view_data['data'] = $data;
           $view_data['page'] = 'member_info.php';
