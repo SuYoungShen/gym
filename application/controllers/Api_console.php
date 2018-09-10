f596148b2d4bc3aa209071bf043c306b72ac2a75
蘇湧盛 <k90218104@gcloud.csu.edu.tw>
2018-09-11 00:51:34 +0800

    排版並新增當月繳款日功能 in 20180911

--- application/controllers/Api_console.php
+++ application/controllers/Api_console.php
@@ -17,7 +17,9 @@ class Api_console extends CI_Controller {
     $this->load->view('console/layout', $view_data);
   }
 
-  // 優惠方案
+  /*****************************
+          優惠方案
+  *****************************/
   public function offer(){
 
     if($this->input->post('rule') == "select"){
@@ -42,32 +44,9 @@ class Api_console extends CI_Controller {
     }
   }
 
-  // 員工專區
-  public function staff(){
-
-    if($this->input->post('rule') == "select"){
-      $id = $this->input->post('id');
-      $where = "id =".'"'.$id.'"';
-      $data = $this->console_model->get_once('staff', $where);
-      $this->output->set_content_type('application/json')->set_output(json_encode($data));
-    }
-
-    if ($this->input->post('rule') == "delete") {
-      $id = $this->input->post('id');
-      $where = "id=".'"'.$id.'"';
-      if($this->console_model->delete_once('staff', $where)){
-        $view_data['code'] = 200;
-        $view_data['msg'] = "刪除成功";
-      }else {
-        $view_data['code'] = 404;
-        $view_data['msg'] = "刪除失敗";
-      }
-      $this->output->set_content_type('application/json')->set_output(json_encode($view_data));
-
-    }
-  }
-
-  // 會員專區
+  /*****************************
+          會員專區
+  *****************************/
   public function member(){
 
     if($this->input->post('rule') == "select"){
@@ -137,5 +116,48 @@ class Api_console extends CI_Controller {
     $this->output->set_content_type('application/json')->set_output(json_encode($data));
 
   }
+
+  /*****************************
+          當月繳款日
+  *****************************/
+  public function month_pay(){
+
+    if($this->input->post('rule') == "select"){
+      $id = $this->input->post('id');
+      $where = "card_id =".'"'.$id.'"';
+      $data = $this->console_model->get_once('member', $where);
+
+      $this->output->set_content_type('application/json')->set_output(json_encode($data));
+    }
+  }
+
+  /*****************************
+          員工專區
+  *****************************/
+  public function staff(){
+
+    if($this->input->post('rule') == "select"){
+      $id = $this->input->post('id');
+      $where = "id =".'"'.$id.'"';
+      $data = $this->console_model->get_once('staff', $where);
+      $this->output->set_content_type('application/json')->set_output(json_encode($data));
+    }
+
+    if ($this->input->post('rule') == "delete") {
+      $id = $this->input->post('id');
+      $where = "id=".'"'.$id.'"';
+      if($this->console_model->delete_once('staff', $where)){
+        $view_data['code'] = 200;
+        $view_data['msg'] = "刪除成功";
+      }else {
+        $view_data['code'] = 404;
+        $view_data['msg'] = "刪除失敗";
+      }
+      $this->output->set_content_type('application/json')->set_output(json_encode($view_data));
+
+    }
+  }
+
+
 }
 ?>
